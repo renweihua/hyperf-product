6f330c94ff986343162bca9434656e12f0d9994c
小丑路人 <2278757482@qq.com>
2021-03-31 15:39:10 +0800

    日志配置

--- hyperf-server/config/autoload/logger.php
+++ hyperf-server/config/autoload/logger.php
@@ -9,21 +9,202 @@ declare(strict_types=1);
  * @contact  group@hyperf.io
  * @license  https://github.com/hyperf/hyperf/blob/master/LICENSE
  */
+
+use Monolog\Formatter\LineFormatter;
+use Monolog\Handler\RotatingFileHandler;
+use Monolog\Logger;
+
 return [
     'default' => [
-        'handler' => [
-            'class' => Monolog\Handler\StreamHandler::class,
-            'constructor' => [
-                'stream' => BASE_PATH . '/runtime/logs/hyperf.log',
-                'level' => Monolog\Logger::DEBUG,
+        //'handler' => [
+        //    'class' => RotatingFileHandler::class,
+        //    'constructor' => [
+        //        'filename' => BASE_PATH . '/runtime/logs/hyperf.log',
+        //        'level' => Logger::DEBUG,
+        //    ],
+        //],
+        //'formatter' => [
+        //    'class' => LineFormatter::class,
+        //    'constructor' => [
+        //        'format' => "||%datetime%||%channel%||%level_name%||%message%||%context%||%extra%\n",
+        //        'dateFormat' => 'Y-m-d H:i:s',
+        //        'allowInlineLineBreaks' => true,
+        //    ],
+        //],
+
+        'handlers' => [
+            [
+                'class'       => RotatingFileHandler::class,
+                'constructor' => [
+                    'filename' => BASE_PATH . '/runtime/logs/hyperf.log',
+                    'level'    => Logger::INFO,
+                ],
+                'formatter'   => [
+                    'class'       => LineFormatter::class,
+                    'constructor' => [
+                        'format'                => "||%datetime%||%channel%||%level_name%||%message%||%context%||%extra%\n",
+                        'dateFormat'            => 'Y-m-d H:i:s',
+                        'allowInlineLineBreaks' => true,
+                    ],
+                ],
+            ],
+            [
+                'class'       => RotatingFileHandler::class,
+                'constructor' => [
+                    'filename' => BASE_PATH . '/runtime/logs/hyperf-debug.log',
+                    'level'    => Logger::DEBUG,
+                ],
+                'formatter'   => [
+                    'class'       => LineFormatter::class,
+                    'constructor' => [
+                        'format'                => "||%datetime%||%channel%||%level_name%||%message%||%context%||%extra%\n",
+                        'dateFormat'            => 'Y-m-d H:i:s',
+                        'allowInlineLineBreaks' => true,
+                    ],
+                ],
+            ],
+            [
+                'class'       => RotatingFileHandler::class,
+                'constructor' => [
+                    'filename' => BASE_PATH . '/runtime/logs/hyperf-notice.log',
+                    'level'    => Logger::NOTICE,
+                ],
+                'formatter'   => [
+                    'class'       => LineFormatter::class,
+                    'constructor' => [
+                        'format'                => "||%datetime%||%channel%||%level_name%||%message%||%context%||%extra%\n",
+                        'dateFormat'            => 'Y-m-d H:i:s',
+                        'allowInlineLineBreaks' => true,
+                    ],
+                ],
+            ],
+            [
+                'class'       => RotatingFileHandler::class,
+                'constructor' => [
+                    'filename' => BASE_PATH . '/runtime/logs/hyperf-warning.log',
+                    'level'    => Logger::WARNING,
+                ],
+                'formatter'   => [
+                    'class'       => LineFormatter::class,
+                    'constructor' => [
+                        'format'                => "||%datetime%||%channel%||%level_name%||%message%||%context%||%extra%\n",
+                        'dateFormat'            => 'Y-m-d H:i:s',
+                        'allowInlineLineBreaks' => true,
+                    ],
+                ],
+            ],
+            [
+                'class'       => RotatingFileHandler::class,
+                'constructor' => [
+                    'filename' => BASE_PATH . '/runtime/logs/hyperf-error.log',
+                    'level'    => Logger::ERROR,
+                ],
+                'formatter'   => [
+                    'class'       => LineFormatter::class,
+                    'constructor' => [
+                        'format'                => "||%datetime%||%channel%||%level_name%||%message%||%context%||%extra%\n",
+                        'dateFormat'            => 'Y-m-d H:i:s',
+                        'allowInlineLineBreaks' => true,
+                    ],
+                ],
+            ],
+            [
+                'class'       => RotatingFileHandler::class,
+                'constructor' => [
+                    'filename' => BASE_PATH . '/runtime/logs/hyperf-critical.log',
+                    'level'    => Logger::CRITICAL,
+                ],
+                'formatter'   => [
+                    'class'       => LineFormatter::class,
+                    'constructor' => [
+                        'format'                => "||%datetime%||%channel%||%level_name%||%message%||%context%||%extra%\n",
+                        'dateFormat'            => 'Y-m-d H:i:s',
+                        'allowInlineLineBreaks' => true,
+                    ],
+                ],
+            ],
+            [
+                'class'       => RotatingFileHandler::class,
+                'constructor' => [
+                    'filename' => BASE_PATH . '/runtime/logs/hyperf-alert.log',
+                    'level'    => Logger::ALERT,
+                ],
+                'formatter'   => [
+                    'class'       => LineFormatter::class,
+                    'constructor' => [
+                        'format'                => "||%datetime%||%channel%||%level_name%||%message%||%context%||%extra%\n",
+                        'dateFormat'            => 'Y-m-d H:i:s',
+                        'allowInlineLineBreaks' => true,
+                    ],
+                ],
+            ],
+            [
+                'class'       => RotatingFileHandler::class,
+                'constructor' => [
+                    'filename' => BASE_PATH . '/runtime/logs/hyperf-emergency.log',
+                    'level'    => Logger::EMERGENCY,
+                ],
+                'formatter'   => [
+                    'class'       => LineFormatter::class,
+                    'constructor' => [
+                        'format'                => "||%datetime%||%channel%||%level_name%||%message%||%context%||%extra%\n",
+                        'dateFormat'            => 'Y-m-d H:i:s',
+                        'allowInlineLineBreaks' => true,
+                    ],
+                ],
             ],
         ],
-        'formatter' => [
-            'class' => Monolog\Formatter\LineFormatter::class,
-            'constructor' => [
-                'format' => null,
-                'dateFormat' => 'Y-m-d H:i:s',
-                'allowInlineLineBreaks' => true,
+    ],
+    'crontab' => [
+        'handlers' => [
+            [
+                'class'       => RotatingFileHandler::class,
+                'constructor' => [
+                    'filename' => BASE_PATH . '/runtime/logs/crontab.log',
+                    'level'    => Logger::INFO,
+                ],
+                'formatter'   => [
+                    'class'       => LineFormatter::class,
+                    'constructor' => [
+                        'format'                => "||%datetime%||%channel%||%level_name%||%message%||%context%||%extra%\n",
+                        'dateFormat'            => 'Y-m-d H:i:s',
+                        'allowInlineLineBreaks' => true,
+                    ],
+                ],
+            ],
+            [
+                'class'       => RotatingFileHandler::class,
+                'constructor' => [
+                    'filename' => BASE_PATH . '/runtime/logs/hyperf-notice.log',
+                    'level'    => Logger::NOTICE,
+                ],
+                'formatter'   => [
+                    'class'       => LineFormatter::class,
+                    'constructor' => [
+                        'format'                => "||%datetime%||%channel%||%level_name%||%message%||%context%||%extra%\n",
+                        'dateFormat'            => 'Y-m-d H:i:s',
+                        'allowInlineLineBreaks' => true,
+                    ],
+                ],
+            ],
+        ],
+    ],
+    'sql' => [
+        'handlers' => [
+            [
+                'class'       => RotatingFileHandler::class,
+                'constructor' => [
+                    'filename' => BASE_PATH . '/runtime/logs/sql.log',
+                    'level'    => Logger::INFO,
+                ],
+                'formatter'   => [
+                    'class'       => LineFormatter::class,
+                    'constructor' => [
+                        'format'                => "||%datetime%||%channel%||%level_name%||%message%||%context%||%extra%\n",
+                        'dateFormat'            => 'Y-m-d H:i:s',
+                        'allowInlineLineBreaks' => true,
+                    ],
+                ],
             ],
         ],
     ],
