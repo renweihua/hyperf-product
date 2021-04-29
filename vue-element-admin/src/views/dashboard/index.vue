9bb5e129edf12ff9b140e308a949c3b033342093
小丑路人 <2278757482@qq.com>
2021-04-30 00:44:30 +0800

    管理后台首页渲染

--- vue-element-admin/src/views/dashboard/index.vue
+++ vue-element-admin/src/views/dashboard/index.vue
@@ -1,27 +1,28 @@
 <template>
-  <div class="dashboard-container">
-    <component :is="currentRole" />
-  </div>
+    <div class="dashboard-container">
+        <component :is="currentRole"/>
+    </div>
 </template>
 
 <script>
-import { mapGetters } from 'vuex'
-import adminDashboard from './admin'
+    import {mapGetters} from 'vuex';
+    import adminDashboard from './admin';
 
-export default {
-  name: 'Dashboard',
-  components: { adminDashboard },
-  data() {
-    return {
-      currentRole: 'adminDashboard'
+    export default {
+        name: 'Dashboard',
+        components: {adminDashboard},
+        data() {
+            return {
+                currentRole: 'adminDashboard',
+            }
+        },
+        computed: {
+            ...mapGetters([
+                'roles'
+            ])
+        },
+        created() {
+
+        }
     }
-  },
-  computed: {
-    ...mapGetters([
-      'roles'
-    ])
-  },
-  created() {
-  }
-}
 </script>
